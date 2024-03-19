00dcd35513d772bcf424ca020689d317d11a9c0b
Benji Wallis <benji@benjiwallis.xyz>
2024-03-19 13:28:59 +1300

    added comments

--- Code-Runner/NCEA Style/TrackSpending.py
+++ Code-Runner/NCEA Style/TrackSpending.py
@@ -1,26 +1,28 @@
+"""Code to track and present spending over time."""
+
+#import libraries
 from word2number import w2n
 
+#initialise variables
 bank = 200
 expenses = [200]
+
 while bank > 0:
   spend = input('Enter the amount spent: ')
-  if spend == '0':
+  if spend == '0': # exit loop if 0 is entered
     break
-  # input validation
-  try:
-    # try to convert word to number
-    try:
+  try: # convert to integer
+    try: # convert word to number
       spend = w2n.word_to_num(spend)
-    except ValueError:
-      # if it fails, try to convert string to int
+    except ValueError: # if not a word, try to convert to integer
       spend = int(spend)
-  except ValueError:
+  except ValueError: # if not a number, ask for a number
     print('Please enter a number.')
     continue
-  if spend < 0:
+  if spend < 0: # check for negative number
     print('You cannot spend a negative amount.')
-  bank = bank - spend
-  expenses.append(bank)
+  bank = bank - spend # update bank balance
+  expenses.append(bank) # add to list of expenses
 
 #print expenses
 print(f'My bank balances have been:')
